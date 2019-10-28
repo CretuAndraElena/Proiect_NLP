eb6c4d23d4f51f4734484808754e78c1dd4b12ca
andcretu@EN1210015 <andcretu@endava.com>
2019-10-28 21:08:35 +0200

    Laboratorul 4

--- Laboratorul3.py
+++ Laboratorul3.py
@@ -31,7 +31,7 @@ def parse_html(link):
     brand = brand.text.strip()
 
     nume = soup.find_all('div', attrs={'class':'title'})
-    nume = nume[6].text.strip()
+    nume = nume[3].text.strip()
 
     print(nume)
     pret_nou = soup.find('span', attrs={'class': 'product-price-value value'})
@@ -43,28 +43,27 @@ def parse_html(link):
     marime = soup.find('span', attrs={'class': 'original-size'})
     marime = marime.text.strip()
 
-    return (brand, nume, pret_nou, pret_vechi, marime)
+    categorie = 'barbati'
 
-
-#Laboratorul 3
-
-quote_pages = extragere_linkuri_produse("https://www.sportvision.ro/produse/barbati")
-
-print(quote_pages)
-produse = []
-
-#extragere informatii din  link-uri
-for quote_page in quote_pages:
-    try:
-        produs = parse_html(quote_page)
-        #adaugare produs
-        produse.append(produs)
-    except AttributeError:
-        print("Invalid Url: ", quote_page)
+    return (brand, nume, pret_nou, pret_vechi, marime, categorie)
 
 #adaugare informatii in fisier
-with open('index.csv', 'a') as csv_file:
-    writer = csv.writer(csv_file)
+def scriere_in_fisier(produs):
+    with open('produse.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([produs[0],produs[1],produs[2],produs[3],produs[4], produs[5], datetime.now()])
 
-    for brand, name, pret_nou, pret_vechi, marime in produse:
-        writer.writerow([brand, name, pret_nou, pret_vechi, marime , datetime.now()])
+#Laboratorul 3
+for i in range(1,11):
+    link = 'https://www.sportvision.ro/incaltaminte/barbati/page-'+ str(i)
+    quote_pages = extragere_linkuri_produse(link)
+
+    #extragere informatii din  link-uri
+    for quote_page in quote_pages:
+        try:
+            produs = parse_html(quote_page)
+            #adaugare produs
+            scriere_in_fisier(produs)
+        except AttributeError:
+            print("Invalid Url: ", quote_page)
+
